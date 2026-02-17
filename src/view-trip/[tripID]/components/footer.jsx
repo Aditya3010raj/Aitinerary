72dc1a140122ec2a301dc9319d92f8a5d6ca043a
Aditya3010raj <adityarajc3003@gmail.com>
2026-02-18 03:16:12 +0530

    change footer

--- src/view-trip/[tripID]/components/footer.jsx
+++ src/view-trip/[tripID]/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div className='my-7'>
-            <h2 className='text-center text-gray-400'>Created with AI Trip Planner ✈️</h2>
+            <h2 className='text-center text-gray-400'>Created with Aitinerary ✈️</h2>
         </div>
     );
 }
